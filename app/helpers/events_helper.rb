c8c45affa1c024083c2e414b2e1b90a213008dcc
Shtymak <vova.shtimak@gmail.com>
2021-01-22 16:36:15 +0200

    Index page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def event_image(image, width: 200, height: 200)
+    image_tag (image.attached? ? image : '/nfd.jpg'), width: width, height: height, class: 'card-img-top'
+  end
 end
