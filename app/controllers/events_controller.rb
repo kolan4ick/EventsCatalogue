65b2b1d2158a4e085e5fe7f60c223c7211b5dc55
Shtymak <vova.shtimak@gmail.com>
2021-01-22 14:16:38 +0200

    Event Controller changed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,5 +3,15 @@ class EventsController < ApplicationController
   end
 
   def show
+      event
+  end
+
+  private
+
+  def event
+      @event ||= Event.find(params[:id])
+  end
+  def event_params
+    params.require(:event).permit(:name, :body, :end_date, :begin_date, :place, :avatar. images:[])
   end
 end
