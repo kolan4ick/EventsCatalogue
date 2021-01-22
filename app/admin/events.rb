139942ce5fb6059fd96c4cbc9f1e626c69d6f6b3
Shtymak <vova.shtimak@gmail.com>
2021-01-22 14:54:17 +0200

    Avtar and images

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -5,7 +5,41 @@ ActiveAdmin.register Event do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-   permit_params :name, :end_date, :begin_date, :body, :place
+   permit_params :name, :end_date, :begin_date, :body, :place, :avatar, images:[]
+   form do |f|
+     f.inputs do
+       f.input :name
+       f.input :body, as: :text
+       f.input :place
+       f.input :begin_date
+       f.input :end_date
+       f.input :avatar, as: :file
+       f.input :images, as: :file, input_html: { multiple: true }
+     end
+   f.actions
+ end
+
+ show do |f|
+     attributes_table do
+       row :name
+       row :body
+       row :place
+       row :begin_date
+       row :end_date
+       row :avatar do |object|
+         image_tag object.photo, width:250
+       end
+       row :images do
+        div do
+          f.images.each do |picture|
+            div do
+              span image_tag picture, width:250
+            end
+          end
+        end
+      end
+     end
+   end
   #
   # or
   #
