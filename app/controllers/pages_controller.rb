fa6a6d5c37a73ebe53d579472334c63c1716225a
Kolan4ick <Kolan4ick@gmail.com>
2021-01-25 21:03:12 +0200

    base locales

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def index
   end
   def archive
-    @events = Event.where('end_date <= ?',Time.now).page(params[:page]).per($PERPAGE).with_attached_images
+    @events = Event.where('end_date < ?',Time.now).page(params[:page]).per($PERPAGE).with_attached_images
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+  scope '/(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
   devise_for :users
   resources :users, only: [:show]
-  get 'events/index'
-  get 'pages/archive'
-  resources :event_subscribes
-  resources :events do
+    get 'events/index'
+    get 'pages/archive'
     resources :event_subscribes
+    resources :events do
+      resources :event_subscribes
+    end
+    # get 'events/index'
+    # get 'events/show'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :events, only: %i[index show]
+    root to: 'pages#index'
   end
   ActiveAdmin.routes(self)
-  #get 'events/index'
-  #get 'events/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :events, only: %i[index show]
-root to: "pages#index"
 end
