f00cc973a5a069ecd5e0f0721c6f30d8101cf5d1
Shtymak <vova.shtimak@gmail.com>
2021-01-27 20:34:53 +0200

    Search fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,24 +8,28 @@ class EventsController < ApplicationController
   end
   def search
     @search = params[:search]
-
     type_filter
     data_filter
     @events = @events.where("place LIKE '%#{params[:search]}%'").page(params[:page]).per($PERPAGE).with_attached_images
   end
   private
+
   def type_filter
-    @price_type = params[:price_type]
-    @events = (@price_type == '' ? Event.all : @events.where(price: @price_type))
+    @price = params[:price_type]
+    if @price ==""
+      @events = Event.all
+    else
+        @events =Event.where(price: @price )
+    end
   end
 
 def data_filter
   @end_date = params[:end_date]
   @begin_date = params[:begin_date]
   if @begin_date != '' || @end_date != ''
-  @events = Event.where('end_date <= ? AND begin_date >= ?', @end_date.to_date, @begin_date.to_date)
+  @events = @events.where('end_date <= ? AND begin_date >= ?', @end_date.to_date, @begin_date.to_date)
 else
-  @events = Event.order(:name).page(params[:page]).per($PERPAGE).with_attached_images
+  @events = @events.order(:name).page(params[:page]).per($PERPAGE).with_attached_images
 end
 end
   def event
